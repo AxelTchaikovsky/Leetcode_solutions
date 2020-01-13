e085eca5ee181e39f025d2b85b879d23f73f289c
AxelTchaikovsky <lzx1999sjtu@gmail.com>
2020-01-13 18:26:22 +0800

    problem10

---
+++ problem10.cpp
@@ -0,0 +1,26 @@
+bool isMatch(char * s, char * p)
+{
+    int i = 0, j = 0;
+    int memo[50][50] = {{0,},};
+    int flag[50][50] = {{0,},};
+
+    bool dp(int i, intj)
+    {
+        bool ret;
+        if(flag[i][j]) return memo[i][j];
+        if(!p[j]) return(!s[i]);
+        bool first = s[i] && (p[j] == s[i] || p[j] == '.');
+        if(p[j+1]== '*'){
+            ret =  dp(i, j+2) || (first && dp(i+1, j));
+        }
+        else{
+            ret = first && dp(i+1, j+1);
+        }
+        memo[i][j] = ret;
+        flag[i][j] = 1;
+        return ret;
+    }
+    
+    return dp(0.0)
+
+}
